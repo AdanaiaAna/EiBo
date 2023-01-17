3c03ff7c5acecb65df15ce870d007b3b6f2b0456
AdanaiaAna <annamariakiki@mail.ru>
2023-01-17 20:40:32 +0100

    Update
    Neue Datei

--- src/application/DjView.java
+++ src/application/DjView.java
@@ -1,10 +1,8 @@
-package scenes;
+package application;
 
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 
 public class DjView extends BorderPane {
@@ -15,12 +13,12 @@ public class DjView extends BorderPane {
 		
 		VBox buttons_1 = new VBox();
 		Button button_1_1 = new Button(); 
-		Button button_1_2 = new Button();
+		Button button_1_2 = new Button("halohalo");
 		Button button_1_3 = new Button();
 		Button button_1_4 = new Button();
 		
 		buttons_1.getChildren().addAll(button_1_1, button_1_2 , button_1_3, button_1_4);
-		//buttons_1.setId("buttons_1");
+		buttons_1.setId("buttons_1");
 
 		button_1_1.setText("Hello World"); 
 		
@@ -37,9 +35,14 @@ public class DjView extends BorderPane {
 		VBox buttons_3 = new VBox();
 		
 		VBox buttons_4 = new VBox();
-		this.getChildren().addAll(buttons_1, buttons_2);
+		//this.getChildren().addAll(buttons_1, buttons_2);
+		
+		this.setCenter(buttons_1);
 	}
 
 }
 
-		
+
+
+
+
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,8 @@
 package application;
 	
-import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 
@@ -16,37 +11,41 @@ public class Main extends Application {
 	
 	 Stage primaryStage;
 	 Pane StartView; 
-	 scenes.DjView djView;
-
+	 Pane DjView;
+	
 	
 	@Override
-	public void start(Stage primaryStage) throws IOException {
+	public void start(Stage primaryStage) {
 		
-		 this.djView = new scenes.DjView();
-		// this.StartView = new StartView();
-		 this.primaryStage = primaryStage;
-		 
-		 Pane root = new Pane();  //Erzeuge Wurzel-Element, dem alle Elemente hinzugefügt werden
-		 Label label = new Label("My Label");
-		 
-		 Scene scene = new Scene(djView, 1280, 800); //erzeuge die Szene mit dem Wurzel-Element
-		 primaryStage.setTitle("DJ Set");
-		 primaryStage.setScene(scene); // setze die Szene als aktuelle darzustellende Szene für die Bühne
-		 
-		 
-		 
-		 scene.setRoot(djView);
-		 
+		this.DjView = new DjView();
 		
-		 primaryStage.show();  // es muss explizit gesagt werden, dass das Fenster sichtbar gemacht werden soll
-		 
-		
-		 
-		 }
-		
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,1200,800);
+			
+			
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			
+			scene.setRoot(DjView);
+			
+			primaryStage.show();
+			
+			
+			
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
-
