5089f36eb5909135848d1c9a18d34bf058d75393
trashyyjay <jamieannanie@gmail.com>
2023-02-03 01:30:24 +0100

    willkommen du profi dj

--- DJ_Set_New/src/scenes/StartView.java
+++ DJ_Set_New/src/scenes/StartView.java
@@ -2,6 +2,8 @@ package scenes;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
@@ -12,13 +14,16 @@ public class StartView extends BorderPane {
 	public StartView() {
 		
 		VBox start_background = new VBox();
+		ImageView image = new ImageView(new Image("/images/StartBild.png"));
+		image.setPreserveRatio(true);
+		image.setFitWidth(1100);
 		start_background.setId("start_background");
 		
 		play = new Button("Play");
 		
 		play.setId("playButton");
 		
-		start_background.getChildren().addAll(play);
+		start_background.getChildren().addAll(image,play);
 		
 		
 		
